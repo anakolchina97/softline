be3a48dfc07ea186d36b2bb0447e107cc8375c32
Annie <anniehartmann97@gmail.com>
2023-04-17 23:08:10 +0300

    Map, Misson

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -16,11 +16,14 @@ class ProjectApp {
 		this.classes = {
 			// Signal: require('./classes/Signal').default,
 		};
-		this.components = {};
+		this.components = {
+			Tabs: require('../../components/tabs/tabs').default,
+		};
 		this.helpers = {};
 		this.modules = {};
 		document.addEventListener('DOMContentLoaded', () => {
 			document.documentElement.classList.remove('_loading');
+			document.querySelectorAll('[data-tabs]').forEach(tabs => new this.components.Tabs(tabs));
 		});
 	}
 }
