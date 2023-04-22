bb78b322b8ad78b9d86aa0dfde4b7b206ccbb080
Annie <anniehartmann97@gmail.com>
2023-04-22 22:14:21 +0300

    Adaptive

--- src/includes/map/map.js
+++ src/includes/map/map.js
@@ -1,10 +1,16 @@
 class Map {
 	constructor() {
+		this.windowWidth = window.innerWidth;
 		this.dropdownBtn = document.querySelector('[data-dropdown-btn]');
 		this.dropdownIcon = document.querySelector('[data-dropdown-icon]');
 		this.dropdownLinks = document.querySelectorAll('[data-dropdown-link]');
+		this.dropdownLists = document.querySelectorAll('[data-dropdown-list]');
 		this.dropdown = document.querySelector('[data-dropdown]');
+
 		this.init();
+		window.addEventListener('resize', () => {
+			this.windowWidth = window.innerWidth;
+		});
 	}
 	init() {
 		this.dropdownBtn.addEventListener('click', () => {
@@ -15,8 +21,21 @@ class Map {
 		this.dropdownLinks.forEach(link => {
 			link.addEventListener('click', e => {
 				e.preventDefault();
-				this.dropdown.classList.remove('_show');
-				this.dropdownIcon.classList.remove('_show');
+				if (this.windowWidth > 768 && this.dropdown.classList.contains('_show')) {
+					this.dropdown.classList.toggle('_show');
+					this.dropdownIcon.classList.toggle('_show');
+				}
+			});
+		});
+
+		this.dropdownLists.forEach(list => {
+			list.addEventListener('click', e => {
+				const children = e.currentTarget.querySelectorAll(
+					'[data-dropdown-list-item]:not(:first-child)'
+				);
+				children.forEach(child => {
+					child.classList.toggle('_show');
+				});
 			});
 		});
 	}
